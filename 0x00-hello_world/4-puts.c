387a47843bcbb44b001f441bb06e7a4358589b2d
ebunoluwa <bpopoola09@gmail.com>
2022-09-08 14:09:44 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#!/bin/bash
 #include <studio.h>
+
 /**
- * main -Entry point
+ * main - entry point
+ *
  * Return: always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}

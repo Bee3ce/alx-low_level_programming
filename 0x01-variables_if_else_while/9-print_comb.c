d91d03dfd877cec05ed6db5ca5439fc1813d3a6a
ebunoluwa <bpopoola09@gmail.com>
2022-09-09 21:14:27 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints all possible combinations of single-digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+		for (i = '0'; i <= '9'; i++)
+	{
+			putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ')'
+		}
+	}
+}

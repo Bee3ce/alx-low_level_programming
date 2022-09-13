799f42a8515f7308df8396851ed9ccea624152f6
ebunoluwa <bpopoola09@gmail.com>
2022-09-13 07:40:40 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: prints _putchar as a message\n.
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int str[] = {80, 100, 115, 112, 85, 90};
+	int count, sz;
+		sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
+}

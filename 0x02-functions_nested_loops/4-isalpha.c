4c604cad8ff134714e779fe4efffa3e960812069
ebunoluwa <bpopoola09@gmail.com>
2022-09-13 11:39:24 -0700

    task5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,11 +8,11 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'a'; c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
-	else if (c >= 'a'; c <= 'z')
+	else if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 		else

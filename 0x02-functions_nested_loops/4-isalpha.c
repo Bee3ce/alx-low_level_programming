3fae42d9b15a971e27c5ea82d38f9d0a84ce2d87
ebunoluwa <bpopoola09@gmail.com>
2022-09-13 11:36:39 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - entry point
+ * @c: the integer value it recieves
+ * Description: checks if it is alphabet
+ * Return: 1 if true. 0 if false
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a'; c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'a'; c <= 'z')
+	{
+		return (1);
+		else
+		{
+			return (0);
+		}
+	}
+}

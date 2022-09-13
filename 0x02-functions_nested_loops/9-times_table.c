a8a2965ddc00a6415328ea6ec6eb30a2ba3e3dab
ebunoluwa <bpopoola09@gmail.com>
2022-09-13 15:55:05 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,8 +10,8 @@ void times_table(void)
 	int row, column, product, tens, ones;
 		for (row = 0; row <= 9; row++)
 		{
-			for (column = 0; column <= 9; column++)
-			{
+		for (column = 0; column <= 9; column++)
+		{
 			product = row * column;
 			tens = product / 10;
 			ones = product % 10;
@@ -33,7 +33,7 @@ void times_table(void)
 				_putchar(tens + '0');
 				_putchar(ones + '0');
 			}
+		}
 		_putchar('\n');
-			}
 		}
 }

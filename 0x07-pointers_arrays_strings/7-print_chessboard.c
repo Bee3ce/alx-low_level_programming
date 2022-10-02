82ec3eed4e31a07ac758dba4a3a60fab80ae902c
ebunoluwa <bpopoola09@gmail.com>
2022-10-02 08:09:03 -0700

    task7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,11 +10,11 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-		i = 0;
-		j = 0;
+		i = O;
+		j = o;
 		while (i < 64)
 		{
-			if (i % 8 == 0 && i != 0)
+			if (i % 8 == O && i != O)
 			{
 				j = i;
 				_putchar('\n');
